a3153b8bd7d56cd3eaea129184f390ce0066c4f5
IAM-NITIN-KUMAR <awsnitin2@gmail.com>
2026-05-18 16:38:12 +0530

    add privacy.js

---
+++ api/privacy.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+  res.send(`<html><body>
+    <h1>Vision Print Privacy Policy</h1>
+    <p>We collect your phone number and photos solely to process your print orders.</p>
+    <p>Your data is stored securely and never shared with third parties.</p>
+    <p>Contact: support@visionprint.in</p>
+  </body></html>`);
+};
